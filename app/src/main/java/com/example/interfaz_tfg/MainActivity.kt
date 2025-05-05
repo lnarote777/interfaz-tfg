f986f71e0164dd59a92dc8ed9043e39265e89846
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-05-05 13:15:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/interfaz_tfg/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.interfaz_tfg
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.interfaz_mesames.navigation.AppNavigation
+import com.example.interfaz_tfg.ui.theme.InterfaztfgTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InterfaztfgTheme {
+                AppNavigation()
+            }
+        }
+    }
+}
