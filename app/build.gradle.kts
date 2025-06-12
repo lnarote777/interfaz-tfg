cf106150e0de67b79d66a6a0bfea82a296c7d031
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-06-12 13:36:17 +0200

    creacion de apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,17 +29,25 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
+
+    lintOptions {
+        disable("NullSafeMutableLiveData")
+    }
 }
+
 
 dependencies {
 
